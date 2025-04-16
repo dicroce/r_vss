d45ed2bf3f8303565b5eef8415bd9b55b9345e61
Tony Di Croce <dicroce@gmail.com>
2025-04-16 08:39:27 -0400

    P

--- GSTREAMERConfig.cmake
+++ GSTREAMERConfig.cmake
@@ -1,25 +1,12 @@
 if(NOT TARGET gstreamer)
 
     if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-        add_library(gstreamer INTERFACE)
-        target_include_directories(gstreamer INTERFACE
-            "$ENV{GST_TOP_DIR}/include/gstreamer-1.0"
-            "$ENV{GST_TOP_DIR}/include/glib-2.0"
-            "$ENV{GST_TOP_DIR}/lib/glib-2.0/include"
-        )
-        target_link_directories(gstreamer INTERFACE "$ENV{GST_TOP_DIR}/lib")
-        target_link_libraries(gstreamer INTERFACE
-            gstreamer-1.0.lib
-            gstapp-1.0.lib
-            gstsdp-1.0.lib
-            gstrtp-1.0.lib
-            gstrtspserver-1.0.lib
-            gstcodecparsers-1.0.lib
-            gobject-2.0.lib
-            gmodule-2.0.lib
-            xml2.lib
-            gthread-2.0.lib
-            glib-2.0.lib
+        add_library(gstreamer INTERFACE IMPORTED GLOBAL)
+        set_target_properties(gstreamer PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "$ENV{GST_TOP_DIR}/include/gstreamer-1.0;$ENV{GST_TOP_DIR}/include/glib-2.0;$ENV{GST_TOP_DIR}/lib/glib-2.0/include"
+            INTERFACE_LINK_DIRECTORIES "$ENV{GST_TOP_DIR}/lib"
+            INTERFACE_LINK_LIBRARIES
+                "gstreamer-1.0.lib;gstapp-1.0.lib;gstsdp-1.0.lib;gstrtp-1.0.lib;gstrtspserver-1.0.lib;gstcodecparsers-1.0.lib;gobject-2.0.lib;gmodule-2.0.lib;xml2.lib;gthread-2.0.lib;glib-2.0.lib"
         )
 
         set(GST_LIBS gstreamer)
@@ -37,19 +24,13 @@ if(NOT TARGET gstreamer)
         pkg_search_module(gstreamer-plugins-good REQUIRED IMPORTED_TARGET gstreamer-plugins-good-1.0)
         pkg_search_module(gstreamer-plugins-bad REQUIRED IMPORTED_TARGET gstreamer-plugins-bad-1.0)
 
-        add_library(gstreamer INTERFACE)
-        target_link_libraries(
-            gstreamer INTERFACE
-            PkgConfig::gstreamer
-            PkgConfig::gstreamer-sdp
-            PkgConfig::gstreamer-app
-            PkgConfig::gstreamer-video
-            PkgConfig::gstreamer-codecparsers
-            PkgConfig::gstreamer-rtsp-server
-        )            
+        add_library(gstreamer INTERFACE IMPORTED GLOBAL)
+        set_target_properties(gstreamer PROPERTIES
+            INTERFACE_LINK_LIBRARIES
+                "PkgConfig::gstreamer;PkgConfig::gstreamer-sdp;PkgConfig::gstreamer-app;PkgConfig::gstreamer-video;PkgConfig::gstreamer-codecparsers;PkgConfig::gstreamer-rtsp-server"
+        )
 
         set(GST_LIBS gstreamer)
-
     endif()
-    
+
 endif()
